95399e5d8fac9f21969040d39f088ac232927d48
Babos Csaba <babos.csaba@yourweb.hu>
2015-09-07 23:20:53 +0200

    Readded my test

--- test/test.js
+++ test/test.js
@@ -1,21 +1,44 @@
-var
-    xml = require('../index.js'),
-    filename = __dirname + '/test.xml';
+var assert = require('assert');
+var request = require('request');
 
-function logXML(data) {
-    console.log(JSON.stringify(data, '   ', '\t'));
-}
+var xml = require('../index.js');
+var filename = __dirname + '/test.xml';
 
+it("should be parse the fixture: test.xml", function() {
+   var parsed = xml.parseFileSync(filename);
+   assert.equal(parsed.name, 'xml')
+   assert.equal(parsed.attrib.attr, 'value');
+   assert.equal(parsed.childs.length, 7);
 
+   // TODO write the other tests
+}); 
 
-console.log("sync mode");
-logXML(xml.parseFileSync(filename));
+it("should parse XML with BOM", function(done) {
+    this.timeout(5000);
 
-logXML(xml.parseString("<xml>from string €</xml>"));
+    var testUrlWithBom = 'http://feeds.podtrac.com/kJVKNvGiQn6Q';
 
+    var testParser = function(str) {
+        try {
+            var parsed = xml.parseString(str);    
+            assert.equal(parsed.name, 'rss');
+            done();
+        } catch(e) {
+            console.log(
+                '[ERR] Error on parsing the document!',
+                e
+            );
+        }
+    };
 
-console.log("async mode");
-xml.parseFile(filename, function(err, root){
-    logXML(root)
+    request.get(testUrlWithBom, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            testParser(body);
+        } else if (error) {
+            console.log({
+                'responseCode': response.statusCode,
+                'error': error
+            });
+        }
+    });
 });
-
